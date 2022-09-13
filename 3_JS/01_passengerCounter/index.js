9107534d5bbb80c77294853cd061aea27527faae
YuriSarmanho <yuri.sarmanho@hotmail.com>
2022-09-13 15:02:23 -0300

    contador

--- 3_JS/01_passengerCounter/index.js
+++ 3_JS/01_passengerCounter/index.js
@@ -1,1 +1,30 @@
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
+
+let count = 0;
+let saveValue = 0;
+let countEl = document.getElementById("count-el");
+
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
+}
+
+function save() {
+  console.log(saveValue);
+  console.log(count);
+
+  if (saveValue != count) {
+    saveValue = count;
+  }
+  return saveValue;
+}
+
+function reset() {
+  count = 0;
+  countEl.innerText = count;
+}
+
+function checkPoint() {
+  count = saveValue;
+  countEl.innerText = saveValue;
+}
