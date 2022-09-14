9b48d24bc7254fc537627d48e97a78f1a64c6f42
YuriSarmanho <yuri.sarmanho@hotmail.com>
2022-09-14 14:03:04 -0300

    db com uma questao add

---
+++ 3_JS/02_QuizApp/js/index.js
@@ -0,0 +1,30 @@
+const db_questions = [
+  {
+    id: 1,
+    question: "Quais dos itens a baixo é um laço de repetição?",
+    answer_options: [
+      {
+        id: 1,
+        answer: "===",
+        isRigth: false,
+      },
+      {
+        id: 2,
+        answer: "!=",
+        isRigth: false,
+      },
+      {
+        id: 3,
+        answer: "while()",
+        isRigth: true,
+      },
+      {
+        id: 4,
+        answer: "if()",
+        isRigth: false,
+      }
+    ]
+  }
+];
+
+console.log(db_questions);
