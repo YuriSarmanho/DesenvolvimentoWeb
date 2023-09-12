5dee783fcc34f2f9e56b8fff4dcdae74d838b162
yuriSarmanho <yuri.sarmanho@hotmail.com>
2023-09-12 15:47:36 -0300

    sql course section 4

---
+++ 4_sql/04-primary-foreing-key/04-insert-self-reference.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (first_name, last_name, supervisor_id)
+VALUES 
+    ('Julie', 'Barnes', NULL),
+    ('Yuri', 'Sarmanho', 1),
+    ('Manuel', 'Lorens', 1);
---
+++ 4_sql/04-primary-foreing-key/07-insert-many-to-many.sql
@@ -0,0 +1,5 @@
+-- INSERT INTO users (first_name)
+-- VALUES ('Yuri'),('Manuel'),('Julie');
+
+INSERT INTO users_friends (user_id, friend_id) 
+VALUES (1,2),(2,3);
